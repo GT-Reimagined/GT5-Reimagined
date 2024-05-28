686e8b7db6c210134080eb063ad137432ac47e4d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-28 19:53:15 -0400

    reverted blast furnace threshold temp changes

--- common/src/main/java/muramasa/gregtech/material/GregTechMaterialEvent.java
+++ common/src/main/java/muramasa/gregtech/material/GregTechMaterialEvent.java
@@ -17,10 +17,10 @@ public class GregTechMaterialEvent extends MaterialEvent<GregTechMaterialEvent> 
     public GregTechMaterialEvent asSolid(int meltingPoint, int blastFurnaceTemp, IMaterialTag... tags) {
         asSolid(meltingPoint, tags);
         GregTechMaterialTags.BLAST_FURNACE_TEMP.add(material, blastFurnaceTemp);
-        if (blastFurnaceTemp >= 1700){
+        if (blastFurnaceTemp >= 1200){
             flags(GregTechMaterialTags.NEEDS_BLAST_FURNACE, HAS_CUSTOM_SMELTING);
         }
-        if (blastFurnaceTemp > 2400) {
+        if (blastFurnaceTemp > 1750) {
             flags(AntimatterMaterialTypes.INGOT_HOT);
         }
         return this;
