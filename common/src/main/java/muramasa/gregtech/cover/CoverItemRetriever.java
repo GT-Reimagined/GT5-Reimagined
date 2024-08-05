cb18e95aa6c67da0d3a6e3497378da4c8e265510
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-08-04 20:57:04 -0400

    fixed crash with item retriever

--- common/src/main/java/muramasa/gregtech/cover/CoverItemRetriever.java
+++ common/src/main/java/muramasa/gregtech/cover/CoverItemRetriever.java
@@ -27,6 +27,7 @@ public class CoverItemRetriever extends BaseCover {
             if (pipe.getLevel().getGameTime() % 20 == 15 && pipe.pipeCapacityCheck()){
                 ArrayList<BlockEntityItemPipe<?>> tUsedPipes = new ArrayList<>();
                 BlockEntity adjacent = pipe.getCachedBlockEntity(this.side);
+                if (adjacent == null) return;
                 PlatformItemHandler to = TesseractCapUtils.INSTANCE.getItemHandler(adjacent, this.side.getOpposite()).orElse(null);
                 if (to == null) return;
                 for (BlockEntityItemPipe<?> p : BlockEntityItemPipe.scanPipes(pipe, new HashMap<>(), 0, true, false).keySet()){
