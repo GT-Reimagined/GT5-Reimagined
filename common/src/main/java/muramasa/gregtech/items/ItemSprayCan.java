4734e2a2fd64e41a1580ee3be60748f4bf7dfbe0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-08-24 23:28:34 -0400

    updated antimatter, did more work on spray cans

--- common/src/main/java/muramasa/gregtech/items/ItemSprayCan.java
+++ common/src/main/java/muramasa/gregtech/items/ItemSprayCan.java
@@ -1,9 +1,11 @@
 package muramasa.gregtech.items;
 
+import muramasa.antimatter.Ref;
 import muramasa.antimatter.blockentity.pipe.BlockEntityPipe;
 import muramasa.antimatter.item.ItemBasic;
 import muramasa.antimatter.pipe.types.FluidPipe;
 import muramasa.antimatter.pipe.types.ItemPipe;
+import muramasa.antimatter.util.Utils;
 import muramasa.gregtech.GTIRef;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.DyeColor;
@@ -14,7 +16,7 @@ public class ItemSprayCan extends ItemBasic<ItemSprayCan> {
     private final DyeColor color;
 
     public ItemSprayCan(DyeColor color) {
-        super(GTIRef.ID, color.getName() + "_spray_can", "spray_cans/");
+        super(GTIRef.ID, color.getName() + "_spray_can", "spray_cans/", new Properties().tab(Ref.TAB_ITEMS).defaultDurability(512));
         this.color = color;
     }
 
@@ -22,8 +24,12 @@ public class ItemSprayCan extends ItemBasic<ItemSprayCan> {
     public InteractionResult useOn(UseOnContext context) {
         BlockEntity be = context.getLevel().getBlockEntity(context.getClickedPos());
         if (!context.getLevel().isClientSide() && be instanceof BlockEntityPipe<?> pipe && (pipe.getPipeType() instanceof FluidPipe || pipe.getPipeType() instanceof ItemPipe)){
-            pipe.setPipeColor(color.getTextColor());
+            float[] textureDiffuseColors = color.getTextureDiffuseColors();
+            int rgb = (((int)(textureDiffuseColors[0] * 255.0F)) << 16) | (((int)(textureDiffuseColors[1] * 255.0F)) << 8) | ((int)(textureDiffuseColors[2] * 255.0F));
+            pipe.setPipeColor(rgb);
+            pipe.checkConnections();
             pipe.sidedSync(true);
+            Utils.damageStack(1, context.getItemInHand(), context.getPlayer());
             return InteractionResult.SUCCESS;
         }
         return super.useOn(context);
