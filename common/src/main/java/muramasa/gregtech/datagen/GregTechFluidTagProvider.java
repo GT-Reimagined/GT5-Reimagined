81756d8e5dc1e367d3b29b3508972ecad8132f2e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-09-01 12:02:08 -0400

    added gt oil to crude oil fluid tag and added crude oil fluid tag to oil fluid tag

--- common/src/main/java/muramasa/gregtech/datagen/GregTechFluidTagProvider.java
+++ common/src/main/java/muramasa/gregtech/datagen/GregTechFluidTagProvider.java
@@ -6,6 +6,8 @@ import muramasa.antimatter.util.TagUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeColor;
 
+import static muramasa.gregtech.data.Materials.Oil;
+
 public class GregTechFluidTagProvider extends AntimatterFluidTagProvider {
     public GregTechFluidTagProvider(String providerDomain, String providerName, boolean replace) {
         super(providerDomain, providerName, replace);
@@ -20,5 +22,7 @@ public class GregTechFluidTagProvider extends AntimatterFluidTagProvider {
             Material waterMixedDye = Material.get("water_mixed_" + dyeName);
             this.tag(TagUtils.getFluidTag(new ResourceLocation(domain, dyeName))).add(chemDye.getLiquid(), waterMixedDye.getLiquid());
         }
+        this.tag(TagUtils.getForgelikeFluidTag("crude_oil")).add(Oil.getLiquid());
+        this.tag(TagUtils.getForgelikeFluidTag("oil")).addTag(TagUtils.getForgelikeFluidTag("crude_oil"));
     }
 }
