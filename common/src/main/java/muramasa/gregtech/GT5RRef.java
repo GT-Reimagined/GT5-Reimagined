f108f046888cbcb5d1553e1cf582092dd4e0a59f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-10-04 20:39:35 -0400

    changed name in GT5RRef

--- common/src/main/java/muramasa/gregtech/GT5RRef.java
+++ common/src/main/java/muramasa/gregtech/GT5RRef.java
@@ -6,7 +6,7 @@ public class GT5RRef {
 
     /** Mod Data **/
     public static final String ID = "gti";
-    public static final String NAME = "GTI";
+    public static final String NAME = "GT5 Reimagined";
     public static final String VERSION = "0.0.1";
     public static final String ANTIMATTER = Ref.ID;
     public static final String ANTIMATTER_SHARED = Ref.SHARED_ID;
