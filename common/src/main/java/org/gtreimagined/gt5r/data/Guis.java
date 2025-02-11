35680b3b9c14f86b1d268d321055a881910237c9
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-02-10 22:28:48 -0500

    added more output slots to electromagnetic seperator

--- common/src/main/java/org/gtreimagined/gt5r/data/Guis.java
+++ common/src/main/java/org/gtreimagined/gt5r/data/Guis.java
@@ -204,7 +204,7 @@ public class Guis {
                 .add(IT_OUT, 107, 16).add(IT_OUT, 125, 16).add(IT_OUT, 143, 16)
                 .add(IT_OUT, 107, 34).add(IT_OUT, 125, 34).add(IT_OUT, 143, 34)
                 .add(ENERGY, 80, 63);
-        ELECTROMAGNETIC_SEPARATOR.add(COMPRESSOR).add(IT_OUT, 125, 25).add(IT_OUT, 143, 25);
+        ELECTROMAGNETIC_SEPARATOR.add(SIFTER);
         DEHYDRATOR.add(IT_IN, 35, 25).add(IT_IN, 53, 25)
                 .add(FL_IN,35,43).add(FL_IN,53,43)
                 .add(IT_OUT, 107, 7).add(IT_OUT, 125, 7).add(IT_OUT, 143, 7)
