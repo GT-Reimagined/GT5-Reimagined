26e66ffb9dd34f859d2233034a57f7d3e4b1d7f2
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-04-11 21:38:42 -0400

    fixed roaster recipes

--- src/main/java/org/gtreimagined/gt5r/loader/machines/RoasterLoader.java
+++ src/main/java/org/gtreimagined/gt5r/loader/machines/RoasterLoader.java
@@ -10,16 +10,17 @@ import static org.gtreimagined.gt5r.data.Materials.*;
 
 public class RoasterLoader {
     public static void init(){
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(2000)).ii(DUST.getMaterialIngredient(Galena, 1)).io(DUST_SMALL.get(Silver), DUST_SMALL.get(Lead)).fo(SulfurDioxide.getGas(500)).add("galena", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(4000)).ii(DUST.getMaterialIngredient(Chalcopyrite, 1)).io(DUST_SMALL.get(Copper), DUST_SMALL.get(Iron)).fo(SulfurDioxide.getGas(1000)).add("chalcopyrite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(3000)).ii(DUST.getMaterialIngredient(Pyrite, 1)).io(DUST_SMALL.get(Iron)).fo(SulfurDioxide.getGas(1000)).add("pyrite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(2000)).ii(DUST.getMaterialIngredient(Tetrahedrite, 1)).io(DUST_SMALL.get(Copper), DUST_SMALL.get(Antimony), DUST_TINY.get(Iron)).fo(SulfurDioxide.getGas(500)).add("tetrahedrite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(6000)).ii(DUST.getMaterialIngredient(Molybdenite, 1)).io(DUST_SMALL.get(Molybdenum)).fo(SulfurDioxide.getGas(200)).add("molybdenite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(8000)).ii(DUST.getMaterialIngredient(Pentlandite, 1)).io(DUST_SMALL.get(Nickel, 2)).fo(SulfurDioxide.getGas(1000)).add("pentlandite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(5000)).ii(DUST.getMaterialIngredient(Stibnite, 1)).io(DUST_SMALL.get(Antimony)).fo(SulfurDioxide.getGas(1000)).add("stibnite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(4000)).ii(DUST.getMaterialIngredient(Sphalerite, 1)).io(DUST_SMALL.get(Zinc, 2)).fo(SulfurDioxide.getGas(1000)).add("sphalerite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(3000)).ii(DUST.getMaterialIngredient(Cobaltite, 1)).io(DUST_SMALL.get(Cobalt), DUST_SMALL.get(Arsenic)).fo(SulfurDioxide.getGas(1000)).add("cobaltite", 512, 5, 0, 3);
-        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(8000)).ii(DUST.getMaterialIngredient(Sulfur, 1)).io(DUST_TINY.get(Sulfur)).fo(SulfurDioxide.getGas(3000)).add("sulfur", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(500)).ii(DUST.getMaterialIngredient(Galena, 1)).io(DUST_SMALL.get(Silver), DUST_SMALL.get(Lead)).fo(SulfurDioxide.getGas(500)).add("galena", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(1375)).ii(DUST.getMaterialIngredient(Chalcopyrite, 1)).io(DUST_SMALL.get(Copper), DUST_SMALL.get(Iron)).fo(SulfurDioxide.getGas(1000)).add("chalcopyrite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(1834)).ii(DUST.getMaterialIngredient(Pyrite, 1)).io(DUST_SMALL.get(Iron)).fo(SulfurDioxide.getGas(1000)).add("pyrite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(938)).ii(DUST.getMaterialIngredient(Tetrahedrite, 1)).io(DUST_SMALL.get(Copper), DUST_SMALL.get(Antimony), DUST_TINY.get(Iron)).fo(SulfurDioxide.getGas(500)).add("tetrahedrite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(1334)).ii(DUST.getMaterialIngredient(Molybdenite, 1)).io(DUST_SMALL.get(Molybdenum)).fo(SulfurDioxide.getGas(200)).add("molybdenite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(942)).ii(DUST.getMaterialIngredient(Pentlandite, 1)).io(DUST_SMALL.get(Nickel, 2)).fo(SulfurDioxide.getGas(1000)).add("pentlandite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(1200)).ii(DUST.getMaterialIngredient(Stibnite, 1)).io(DUST_SMALL.get(Antimony)).fo(SulfurDioxide.getGas(1000)).add("stibnite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(1000)).ii(DUST.getMaterialIngredient(Sphalerite, 1)).io(DUST_SMALL.get(Zinc, 2)).fo(SulfurDioxide.getGas(1000)).add("sphalerite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(667)).ii(DUST.getMaterialIngredient(Cobaltite, 1)).io(DUST_SMALL.get(Cobalt), DUST_SMALL.get(Arsenic)).fo(SulfurDioxide.getGas(1000)).add("cobaltite", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Air.getGas(8000)).ii(DUST.getMaterialIngredient(Sulfur, 1)).fo(SulfurDioxide.getGas(3000)).add("sulfur_air", 512, 5, 0, 3);
+        RecipeMaps.ROASTER.RB().fi(Oxygen.getGas(2000)).ii(DUST.getMaterialIngredient(Sulfur, 1)).fo(SulfurDioxide.getGas(3000)).add("sulfur", 512, 5, 0, 3);
         RecipeMaps.ROASTER.RB().ii(of(DUST.getMaterialTag(Calcite), 5)).fo(CarbonDioxide.getGas(3000)).io(DUST.get(Quicklime, 2)).add("quicklime",240, 30);
     }
 }
