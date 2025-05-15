9e7739da6f4aba0b5002e4f7f0fe3b529893818c
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-14 21:09:31 -0400

    made Elements no longer reference fields from Element

--- src/main/java/org/gtreimagined/gt5r/data/Elements.java
+++ src/main/java/org/gtreimagined/gt5r/data/Elements.java
@@ -3,17 +3,11 @@ package org.gtreimagined.gt5r.data;
 import org.gtreimagined.gtlib.material.Element;
 
 public class Elements {
-    public static Element Ma = Element.Ma;
-    public static Element Nt = Element.Nt;
-    //bullshit elements from stargate
-    public static Element Nq = Element.Naq;
-    public static Element Ke = Element.Ke;
-
-    /*public static Element Ma = new Element(0, 0, 100, 1,1,"magic", "Ma",-1,null,null, null,  false);
+    public static Element Ma = new Element(0, 0, 100, 1,1,"magic", "Ma",-1,null,null, null,  false);
     public static Element Nt = new Element(0, 100000, 0, 1,1,"neutronium", "Nt",-1,null,null, null,  false);
-    //bullshit elements from stargate
+    //SciFi elements
     public static Element Nq = new Element(174, 352, 0, 1, 1, "naquadah", "Nq", -1, null, null, null, false);
-    public static Element Ke = new Element(126, 192, 0, 1, 1, "trinium", "Ke", -1, null, null, null, false);*/
+    public static Element Ke = new Element(126, 192, 0, 1, 1, "trinium", "Ke", -1, null, null, null, false);
     public static Element Ad = new Element(222, 580, 0, 1, 1, "adamantium", "Ad", -1, null, null, null, false);
 
     public static Element Nq522 = new Element(174, 348, 0, 1, 1, "naquadria", "Nq522", -1, null, null, null, true);
