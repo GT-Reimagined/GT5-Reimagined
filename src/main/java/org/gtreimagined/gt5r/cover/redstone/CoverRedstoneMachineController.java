da477a85b3708f5602105082fb4c77c2cc07b968
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-25 11:27:45 -0400

    fixed redstone machine controller not properly updating shutter cover when mode is changed

--- src/main/java/org/gtreimagined/gt5r/cover/redstone/CoverRedstoneMachineController.java
+++ src/main/java/org/gtreimagined/gt5r/cover/redstone/CoverRedstoneMachineController.java
@@ -1,12 +1,18 @@
 package org.gtreimagined.gt5r.cover.redstone;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import org.gtreimagined.gt5r.cover.CoverShutter;
 import org.gtreimagined.gtlib.blockentity.BlockEntityMachine;
 import org.gtreimagined.gtlib.capability.ICoverHandler;
 import org.gtreimagined.gtlib.cover.CoverFactory;
+import org.gtreimagined.gtlib.cover.ICover;
 import org.gtreimagined.gtlib.machine.MachineState;
 import org.gtreimagined.gtlib.machine.Tier;
 import net.minecraft.core.Direction;
 import org.gtreimagined.gt5r.cover.base.CoverBasicRedstoneInput;
+import org.gtreimagined.gtlib.tool.GTToolType;
 import org.jetbrains.annotations.Nullable;
 
 
@@ -25,6 +31,19 @@ public class CoverRedstoneMachineController extends CoverBasicRedstoneInput {
                 }
             }
         }
+    }
+
+    @Override
+    public InteractionResult onInteract(Player player, InteractionHand hand, Direction side, @Nullable GTToolType type) {
+        InteractionResult interactionResult = super.onInteract(player, hand, side, type);
+        if (interactionResult == InteractionResult.SUCCESS){
+            for (ICover iCover : handler.getAll()) {
+                if (iCover instanceof CoverShutter shutter){
+                    shutter.onBlockUpdateAllSides();
+                }
+            }
+        }
+        return interactionResult;
     }
 
     @Override
