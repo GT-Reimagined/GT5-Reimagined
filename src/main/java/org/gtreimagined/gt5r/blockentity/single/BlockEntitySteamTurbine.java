2ccedeabb63dd628ec203fe89021e3594858a050
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-08-08 14:53:32 -0400

    fixed steam turbine outputting eu to any side

--- src/main/java/org/gtreimagined/gt5r/blockentity/single/BlockEntitySteamTurbine.java
+++ src/main/java/org/gtreimagined/gt5r/blockentity/single/BlockEntitySteamTurbine.java
@@ -1,13 +1,14 @@
 package org.gtreimagined.gt5r.blockentity.single;
 
 import org.gtreimagined.gtlib.blockentity.BlockEntityMachine;
+import org.gtreimagined.gtlib.blockentity.single.BlockEntityGenerator;
 import org.gtreimagined.gtlib.capability.machine.MachineRecipeHandler;
 import org.gtreimagined.gtlib.machine.types.Machine;
 import org.gtreimagined.gtlib.recipe.IRecipe;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class BlockEntitySteamTurbine extends BlockEntityMachine<BlockEntitySteamTurbine> {
+public class BlockEntitySteamTurbine extends BlockEntityGenerator<BlockEntitySteamTurbine> {
     public BlockEntitySteamTurbine(Machine<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
         this.recipeHandler.set(() -> new MachineRecipeHandler<>(this){
