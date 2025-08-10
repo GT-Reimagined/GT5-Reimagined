f75a3e22e1164e643d437685231885e5cf6a1635
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-08-10 15:41:48 -0400

    fixed crash from recipe type registration

--- src/main/java/org/gtreimagined/gt5r/data/GT5RRecipeTypes.java
+++ src/main/java/org/gtreimagined/gt5r/data/GT5RRecipeTypes.java
@@ -1,12 +1,14 @@
 package org.gtreimagined.gt5r.data;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.gtreimagined.gt5r.GT5Reimagined;
 import org.gtreimagined.gt5r.machine.recipe.FusionRecipe;
 
 public class GT5RRecipeTypes {
-    public static final RecipeType<FusionRecipe> FUSION_RECIPE = RecipeType.register(GT5Reimagined.ID + ":fusion");
+    public static final RecipeType<FusionRecipe> FUSION_RECIPE = RecipeType.simple(new ResourceLocation(GT5Reimagined.ID, "fusion"));
     public static void init(){
-
+        ForgeRegistries.RECIPE_TYPES.register(new ResourceLocation(GT5Reimagined.ID, "fusion"), FUSION_RECIPE);
     }
 }
