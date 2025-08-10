bc3dbec1de993b47f08a0fc8e4780d950aa51d8a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-08-10 19:03:46 -0400

    made salt water recipe match gt5u

--- src/main/java/org/gtreimagined/gt5r/loader/machines/DehydratorLoader.java
+++ src/main/java/org/gtreimagined/gt5r/loader/machines/DehydratorLoader.java
@@ -10,7 +10,7 @@ import static org.gtreimagined.gtlib.data.GTMaterialTypes.SMALL_DUST;
 public class DehydratorLoader {
     public static void init() {
         DEHYDRATOR.RB().ii(DUST.getMaterialIngredient(TungsticAcid, 7)).io(DUST.get(TungstenTrioxide, 4)).fo(DistilledWater.getLiquid(3000)).add("tungsten_trioxide", 300 * 20, 16);
-        DEHYDRATOR.RB().fi(SaltWater.getFluidIngredient(1000)).io(SMALL_DUST.get(Salt, 1)).fo(DistilledWater.getLiquid(1000)).add("salt_water_drying", 8 * 20, 16);
+        DEHYDRATOR.RB().fi(SaltWater.getFluidIngredient(1000)).io(SMALL_DUST.get(Salt, 1)).fo(DistilledWater.getLiquid(750)).add("salt_water_drying", 8 * 20, 16);
         DEHYDRATOR.RB().fi(Water.getLiquid(10)).fo(DistilledWater.getLiquid(8)).add("water_drying", 16, 16);
         if (GT5RConfig.HARDER_ALUMINIUM_PROCESSING.get()) {
             DEHYDRATOR.RB().ii(DUST.getMaterialIngredient(AluminiumHydroxide, 14)).io(DUST.get(Alumina, 5)).fo(DistilledWater.getLiquid(9000)).add("alumina", 200 * 20, 16);
