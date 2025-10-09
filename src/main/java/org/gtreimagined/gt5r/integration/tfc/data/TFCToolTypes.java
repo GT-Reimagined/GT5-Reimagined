dd3637dfb6385f838cc7e52b7f0a451858a6c027
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-10-09 15:16:29 -0400

    fixed attack damage and attack speed of javelins

--- src/main/java/org/gtreimagined/gt5r/integration/tfc/data/TFCToolTypes.java
+++ src/main/java/org/gtreimagined/gt5r/integration/tfc/data/TFCToolTypes.java
@@ -1,6 +1,7 @@
 package org.gtreimagined.gt5r.integration.tfc.data;
 
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.block.Blocks;
 import org.gtreimagined.gt5r.GT5Reimagined;
 import org.gtreimagined.gt5r.integration.tfc.item.MaterialJavelin;
 import org.gtreimagined.gtlib.GTAPI;
@@ -14,7 +15,7 @@ import static org.gtreimagined.gtlib.material.MaterialTags.FLINT;
 
 public class TFCToolTypes {
     //public static final GTToolType PROSPECTING_PICK = GTAPI.register(GTToolType.class, new GTToolType(GT5Reimagined.ID, "prospecting_pick", 1, 2, 10, 1.0F, -2.8F, false)).setHasContainer(false).setMaterialTypeItem(PICKAXE_HEAD).setMaterialTypeItemPredicate(m -> !m.has(FLINT));
-    public static final GTToolType JAVELIN = GTAPI.register(GTToolType.class, new GTToolType(GT5Reimagined.ID, "javelin", 2, 1, 10, 2.0f, -2.8f, false)).setUseAction(UseAnim.SPEAR).setToolSupplier(MaterialJavelin::new);
+    public static final GTToolType JAVELIN = GTAPI.register(GTToolType.class, new GTToolType(GT5Reimagined.ID, "javelin", 2, 1, 10, 3.0f, -2.2f, false)).addEffectiveBlocks(Blocks.COBWEB).setUseAction(UseAnim.SPEAR).setToolSupplier(MaterialJavelin::new);
 
     public static void init(){
 
