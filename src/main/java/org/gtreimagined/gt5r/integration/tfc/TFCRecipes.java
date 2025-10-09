91d65dfe5ee4b15033e96e0fae9cc941a3f59b8e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-10-09 16:59:16 -0400

    added javelin heads

--- src/main/java/org/gtreimagined/gt5r/integration/tfc/TFCRecipes.java
+++ src/main/java/org/gtreimagined/gt5r/integration/tfc/TFCRecipes.java
@@ -10,12 +10,15 @@ import net.dries007.tfc.common.recipes.TFCRecipeSerializers;
 import net.dries007.tfc.compat.jei.category.QuernRecipeCategory;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import org.gtreimagined.gt5r.GT5Reimagined;
 import org.gtreimagined.gt5r.data.Materials;
+import org.gtreimagined.gtcore.GTCore;
 import org.gtreimagined.gtlib.data.GTMaterialTypes;
 import org.gtreimagined.gtlib.datagen.GTLibDynamics;
 import org.gtreimagined.gtlib.datagen.providers.GTRecipeProvider;
@@ -24,8 +27,14 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
+import static com.google.common.collect.ImmutableMap.of;
 import static org.gtreimagined.gt5r.data.Materials.Flint;
+import static org.gtreimagined.gt5r.integration.tfc.data.TFCToolTypes.JAVELIN;
+import static org.gtreimagined.gt5r.integration.tfc.data.TFCToolTypes.JAVELIN_HEAD;
+import static org.gtreimagined.gtlib.data.GTLibMaterials.Wood;
 import static org.gtreimagined.gtlib.data.GTMaterialTypes.*;
+import static org.gtreimagined.gtlib.data.GTTools.KNIFE;
+import static org.gtreimagined.gtlib.material.MaterialTags.TOOLS;
 
 public class TFCRecipes {
     public static void initRecipes(Consumer<FinishedRecipe> consumer, GTRecipeProvider provider){
@@ -108,6 +117,25 @@ public class TFCRecipes {
                         " X "
                 },
                 Ingredient.of(Items.FLINT), SHOVEL_HEAD.get(Flint, 1)));
+        consumer.accept(new RockKnappingFinishedRecipe(new ResourceLocation(GT5Reimagined.ID, "rock_knapping/flint_javelin_head"),
+                new String[]{
+                        "XXX  ",
+                        "XXXX ",
+                        "XXXXX",
+                        " XXX ",
+                        "  X  "
+                },
+                Ingredient.of(Items.FLINT), JAVELIN_HEAD.get(Flint, 1)));
+        TOOLS.getAll().forEach((m, t) -> {
+            if (t.toolTypes().contains(JAVELIN)){
+                TagKey<Item> rod = t.handleMaterial().has(ROD) ? ROD.getMaterialTag(t.handleMaterial()) : ROD.getMaterialTag(Wood);
+                provider.addStackRecipe(consumer, GT5Reimagined.ID, m.getId() + "_javelin_from_javelin_head", "gt_tools_from_tool_parts", JAVELIN.getToolStack(m), of('T', JAVELIN_HEAD.getMaterialTag(m), 'R', rod), "T", "R");
+            }
+            if (t.toolTypes().contains(KNIFE)){
+                TagKey<Item> rod = t.handleMaterial().has(ROD) ? ROD.getMaterialTag(t.handleMaterial()) : ROD.getMaterialTag(Wood);
+                provider.addStackRecipe(consumer, GT5Reimagined.ID, m.getId() + "_knife_from_knife_head", "gt_tools_from_tool_parts", KNIFE.getToolStack(m), of('T', KNIFE_HEAD.getMaterialTag(m), 'R', rod), "T", "R");
+            }
+        });
     }
 
     private record QuernFinishedRecipe(ResourceLocation id, Ingredient input, ItemStack output) implements FinishedRecipe {
