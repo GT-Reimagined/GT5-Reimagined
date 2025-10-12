6d26bd20051d5987556d4d5d88bdbd36c5c3b8cc
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-10-11 21:40:48 -0400

    made forge:rods/wood inherit forge:rods/wooden tag
    now tool recipes can use tfc twigs

--- src/main/java/org/gtreimagined/gt5r/datagen/GT5RItemTagProvider.java
+++ src/main/java/org/gtreimagined/gt5r/datagen/GT5RItemTagProvider.java
@@ -1,5 +1,6 @@
 package org.gtreimagined.gt5r.datagen;
 
+import net.minecraftforge.common.Tags;
 import org.gtreimagined.gtlib.GTAPI;
 import org.gtreimagined.gtlib.Ref;
 import org.gtreimagined.gtlib.data.ForgeTags;
@@ -25,6 +26,7 @@ public class GT5RItemTagProvider extends GTItemTagProvider {
     protected void processTags(String domain) {
         super.processTags(domain);
         //this.tag(GT5RTags.CIRCUITS_EXTREME).add(GT5RData.CircuitDataStorage);
+        this.tag(ROD.getMaterialTag(Wood)).addTag(Tags.Items.RODS_WOODEN);
         this.tag(CIRCUITS_ELITE).add(GT5RItems.NanoProcessor);
         this.tag(CIRCUITS_MASTER).add(GT5RItems.QuantumProcessor);
         this.tag(GT5RTags.RESISTORS).add(GT5RItems.Resistor, GT5RItems.SMDResistor);
