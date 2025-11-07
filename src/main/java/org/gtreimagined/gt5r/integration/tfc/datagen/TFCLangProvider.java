bcc89ed69da8cc22595dfd4e013cb1fc0ac21765
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-11-07 16:59:07 -0500

    fixed lang of gt ore prospecting with tfc

--- src/main/java/org/gtreimagined/gt5r/integration/tfc/datagen/TFCLangProvider.java
+++ src/main/java/org/gtreimagined/gt5r/integration/tfc/datagen/TFCLangProvider.java
@@ -3,9 +3,13 @@ package org.gtreimagined.gt5r.integration.tfc.datagen;
 import org.gtreimagined.gt5r.integration.tfc.ore.GTTFCOreBlock;
 import org.gtreimagined.gt5r.integration.tfc.ore.GTTFCOreItem;
 import org.gtreimagined.gtlib.GTAPI;
+import org.gtreimagined.gtlib.data.GTMaterialTypes;
 import org.gtreimagined.gtlib.datagen.providers.GTLanguageProvider;
+import org.gtreimagined.gtlib.ore.BlockOre;
 
-import static org.gtreimagined.gtlib.util.Utils.lowerUnderscoreToUpperSpaced;
+import static org.gtreimagined.gtlib.util.Utils.*;
+import static org.gtreimagined.gtlib.util.Utils.getLocalizeStoneType;
+import static org.gtreimagined.gtlib.util.Utils.getLocalizedType;
 
 public class TFCLangProvider extends GTLanguageProvider {
     public TFCLangProvider(String providerDomain, String providerName, String locale) {
@@ -19,6 +23,10 @@ public class TFCLangProvider extends GTLanguageProvider {
         });
         GTAPI.all(GTTFCOreBlock.class, domain).forEach(i -> {
             add(i, lowerUnderscoreToUpperSpaced(i.getId()));
+        });
+        GTAPI.all(BlockOre.class, o -> {
+            String nativeSuffix = o.getMaterial().getElement() != null ? "Native " : "";
+            add(o.getDescriptionId() + ".prospected", String.join("", nativeSuffix, getLocalizedType(o.getMaterial())));
         });
     }
 }
