cdaaf3a6095727379a16e30df73d159faf1e3c2e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-02-06 16:09:04 -0500

    fixed ie ore removals

--- src/main/java/org/gtreimagined/gt5r/integration/IERegistrar.java
+++ src/main/java/org/gtreimagined/gt5r/integration/IERegistrar.java
@@ -1,24 +1,32 @@
 package org.gtreimagined.gt5r.integration;
 
+import blusunrize.immersiveengineering.common.world.IEOreFeature.IEOreFeatureConfig;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biome.ClimateSettings;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.MobSpawnSettings.Builder;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.common.world.BiomeModifier.Phase;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.gtreimagined.gtlib.GTAPI;
 import org.gtreimagined.gtlib.GTMod;
 import org.gtreimagined.gtlib.registration.RegistrationEvent;
 import org.gtreimagined.gtlib.util.RegistryUtils;
 import org.gtreimagined.gtlib.worldgen.GTLibWorldGenerator;
 import org.gtreimagined.gtlib.worldgen.IGTWorldgenFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IERegistrar extends GTMod implements IGTWorldgenFunction {
     @Override
@@ -43,7 +51,13 @@ public class IERegistrar extends GTMod implements IGTWorldgenFunction {
         if (phase == Phase.REMOVE){
             String[] oreTypes = new String[]{"aluminum", "lead", "silver", "nickel", "uranium"};
             for (String oreType : oreTypes){
-                GTLibWorldGenerator.removeDecoratedFeatureFromAllBiomes(gen, Decoration.UNDERGROUND_ORES, Feature.ORE, ib("ore_" + oreType).defaultBlockState(), ib("deepslate_ore_" + oreType).defaultBlockState());
+                List<BlockState> blockStates = List.of(ib("ore_" + oreType).defaultBlockState(), ib("deepslate_ore_" + oreType).defaultBlockState());
+                gen.getFeatures(Decoration.UNDERGROUND_ORES).removeIf(f -> {
+                    if (f.get().feature().get().config() instanceof IEOreFeatureConfig oreFeatureConfig){
+                        return oreFeatureConfig.targetList().stream().anyMatch(t -> blockStates.contains(t.state));
+                    }
+                    return false;
+                });
             }
         }
     }
