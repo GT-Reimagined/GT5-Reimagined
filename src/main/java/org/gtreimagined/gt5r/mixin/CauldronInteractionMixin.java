684bf7a0cd49482d841589af5e0f438e404c7ae5
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-02-08 16:48:50 -0500

    added mixin method to make bottle not turn bronze cauldron into vanilla cauldron

--- src/main/java/org/gtreimagined/gt5r/mixin/CauldronInteractionMixin.java
+++ src/main/java/org/gtreimagined/gt5r/mixin/CauldronInteractionMixin.java
@@ -3,6 +3,7 @@ package org.gtreimagined.gt5r.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
@@ -29,6 +30,21 @@ import java.util.function.Predicate;
 
 @Mixin(CauldronInteraction.class)
 public interface CauldronInteractionMixin {
+    @Inject(method = "lambda$bootStrap$8", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"), cancellable = true)
+    private static void gt5r$injectEmptyBottle(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, ItemStack stack, CallbackInfoReturnable<InteractionResult> cir){
+        if (state.is(GT5RBlocks.BRONZE_CAULDRON)){
+            Item item = stack.getItem();
+            player.setItemInHand(hand, ItemUtils.createFilledResult(stack, player, new ItemStack(Items.GLASS_BOTTLE)));
+            player.awardStat(Stats.FILL_CAULDRON);
+            player.awardStat(Stats.ITEM_USED.get(item));
+            level.setBlockAndUpdate(pos, GT5RBlocks.BRONZE_WATER_CAULDRON.defaultBlockState());
+            level.playSound(null, pos, SoundEvents.BOTTLE_EMPTY, SoundSource.BLOCKS, 1.0F, 1.0F);
+            level.gameEvent(null, GameEvent.FLUID_PLACE, pos);
+            cir.setReturnValue(InteractionResult.SUCCESS);
+        }
+
+    }
+
     @Inject(method = "fillBucket", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"), cancellable = true)
     private static void gt5r$injectFillBucket(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand hand, ItemStack emptyStack, ItemStack filledStack, Predicate<BlockState> statePredicate, SoundEvent fillSound, CallbackInfoReturnable<InteractionResult> cir){
         if (blockState.is(GT5RBlocks.BRONZE_WATER_CAULDRON)){
