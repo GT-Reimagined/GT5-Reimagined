3994fe4e194fd62811fce9e34add7fa9303aa69c
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-02-26 20:40:13 -0500

    added ExperienceOrb mixin in prep for mending nerf

---
+++ src/main/java/org/gtreimagined/gt5r/mixin/ExperienceOrbMixin.java
@@ -0,0 +1,8 @@
+package org.gtreimagined.gt5r.mixin;
+
+import net.minecraft.world.entity.ExperienceOrb;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ExperienceOrb.class)
+public class ExperienceOrbMixin {
+}
