85f7187652bc8a12a0aaf091ee249d6ff0a48986
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-02 17:49:03 -0500

    added item output slot to fluid press

--- src/main/java/org/gtreimagined/gt5r/data/Guis.java
+++ src/main/java/org/gtreimagined/gt5r/data/Guis.java
@@ -169,8 +169,8 @@ public class Guis {
         REPLICATOR.add(COMPRESSOR).add(FL_IN, 44, 63).add(FL_IN, 62, 63).add(FL_OUT, 107, 63);
         ROCK_BREAKER.add(COMPRESSOR);
         FERMENTER.add(FLUID_CANNER);
-        FLUID_PRESS.add(IT_IN, 53, 25).add(ENERGY, 80, 63).add(FL_OUT, 107, 63);
-        SMELTER.add(FLUID_PRESS);
+        FLUID_PRESS.add(COMPRESSOR).add(FL_OUT, 107, 63);
+        SMELTER.add(IT_IN, 53, 25).add(ENERGY, 80, 63).add(FL_OUT, 107, 63);
         FLUID_HEATER.add(ENERGY, 80, 63).add(FL_IN, 53, 63).add(FL_OUT, 107, 63);
         FLUID_SOLIDIFIER.add(COMPRESSOR).add(FL_IN, 53, 63);
         SCANNER.add(FLUID_SOLIDIFIER).add(IT_IN, 125, 63);
