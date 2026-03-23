1ff6adcebf4cbf4c95ae427b506b4686ce038624
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-23 15:59:45 -0400

    boosted speed of steel and wrought iron in pbf

--- src/main/java/org/gtreimagined/gt5r/loader/multi/BlastFurnaceLoader.java
+++ src/main/java/org/gtreimagined/gt5r/loader/multi/BlastFurnaceLoader.java
@@ -24,10 +24,10 @@ import static org.gtreimagined.gtlib.recipe.ingredient.RecipeIngredient.of;
 public class BlastFurnaceLoader {
     public static void init() {
         /* PRIMITIVE */
-        PRIMITIVE_BLAST_FURNACE.RB().ii(INGOT.getMaterialIngredient(WroughtIron,1)).io(INGOT.get(Steel, 1), SMALL_DUST.get(DarkAsh,8)).outputChances(1.0, 0.5).add("steel_ingot",7200);
-        PRIMITIVE_BLAST_FURNACE.RB().ii(DUST.getMaterialIngredient(Iron,1)).io(INGOT.get(WroughtIron, 1)).add("wrought_iron_ingot_from_dust",3600);
+        PRIMITIVE_BLAST_FURNACE.RB().ii(INGOT.getMaterialIngredient(WroughtIron,1)).io(INGOT.get(Steel, 1), SMALL_DUST.get(DarkAsh,8)).outputChances(1.0, 0.5).add("steel_ingot",3600);
+        PRIMITIVE_BLAST_FURNACE.RB().ii(DUST.getMaterialIngredient(Iron,1)).io(INGOT.get(WroughtIron, 1)).add("wrought_iron_ingot_from_dust",1800);
         if (!GTAPI.isModLoaded("tfc")){
-            PRIMITIVE_BLAST_FURNACE.RB().ii(INGOT.getMaterialIngredient(Iron,1)).io(INGOT.get(WroughtIron, 1)).add("wrought_iron_ingot",3600);
+            PRIMITIVE_BLAST_FURNACE.RB().ii(INGOT.getMaterialIngredient(Iron,1)).io(INGOT.get(WroughtIron, 1)).add("wrought_iron_ingot",1800);
         }
         DUST.all().forEach(m -> {
             if (m.has(GT5RMaterialTags.NEEDS_BLAST_FURNACE) && m.has(GT5RMaterialTags.BLAST_FURNACE_TEMP)){
