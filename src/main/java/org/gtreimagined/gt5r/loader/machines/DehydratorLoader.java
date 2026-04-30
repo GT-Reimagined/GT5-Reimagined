d33c32b6e3a1add7afb1b4f0a642619fa23e888e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-04-30 18:21:44 -0400

    added monazite to helium recipe to dehydrator

--- src/main/java/org/gtreimagined/gt5r/loader/machines/DehydratorLoader.java
+++ src/main/java/org/gtreimagined/gt5r/loader/machines/DehydratorLoader.java
@@ -12,6 +12,7 @@ public class DehydratorLoader {
         DEHYDRATOR.RB().ii(DUST.getMaterialIngredient(TungsticAcid, 7)).io(DUST.get(TungstenTrioxide, 4)).fo(DistilledWater.getLiquid(3000)).add("tungsten_trioxide", 300 * 20, 16);
         DEHYDRATOR.RB().fi(SaltWater.getFluidIngredient(1000)).io(SMALL_DUST.get(Salt, 1)).fo(DistilledWater.getLiquid(750)).add("salt_water_drying", 8 * 20, 16);
         DEHYDRATOR.RB().fi(Water.getLiquid(10)).fo(DistilledWater.getLiquid(8)).add("water_drying", 16, 16);
+        DEHYDRATOR.RB().ii(DUST.getMaterialIngredient(Monazite, 1)).io(DUST.get(RareEarth)).fo(Helium.getGas(500)).outputChances(.5).add("monazite_to_helium", 64 * 64, 64);
         if (GT5RConfig.HARDER_ALUMINIUM_PROCESSING.get()) {
             DEHYDRATOR.RB().ii(DUST.getMaterialIngredient(AluminiumHydroxide, 14)).io(DUST.get(Alumina, 5)).fo(DistilledWater.getLiquid(9000)).add("alumina", 200 * 20, 16);
         }
