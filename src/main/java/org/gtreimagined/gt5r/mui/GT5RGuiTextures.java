32ed757d7ad7a928a914ca610a0ee3c900ecdf98
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-05-20 22:17:16 -0400

    separated slot textures into overlay textures, added background textures for tanks and multiblocks

---
+++ src/main/java/org/gtreimagined/gt5r/mui/GT5RGuiTextures.java
@@ -0,0 +1,18 @@
+package org.gtreimagined.gt5r.mui;
+
+import brachy.modularui.drawable.UITexture;
+import org.gtreimagined.gt5r.GT5Reimagined;
+
+public class GT5RGuiTextures {
+    public static final UITexture MULTIBLOCK_BACKGROUND = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/widgets/multiblock_background.png").imageSize(143, 75).build();
+    public static final UITexture TANK_BACKGROUND = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/widgets/basic_tank_background").imageSize(71, 45).build();
+
+    public static final UITexture BATTERY_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/battery.png").colorType(null).build();
+    public static final UITexture BLUEPRINT_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/blueprint.png").build();
+    public static final UITexture CRAFTING_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/crafting.png").build();
+    public static final UITexture PRIMITIVE_CELL_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/primitive_cell.png").build();
+    public static final UITexture PRIMITIVE_DUST_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/primitive_dust.png").build();
+    public static final UITexture PRIMITIVE_FIRE_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/primitive_fire.png").build();
+    public static final UITexture PRIMITIVE_INGOT_SLOT_OVERLAY = UITexture.builder().location(GT5Reimagined.ID, "textures/gui/slots/overlays/primitive_ingot.png").build();
+
+}
