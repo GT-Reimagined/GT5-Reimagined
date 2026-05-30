0ce2d08b55b672801bd6912487b45ec08b754fd3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-05-29 21:09:14 -0400

    removed some old widget code and other old gui code

--- src/main/java/org/gtreimagined/gt5r/data/ClientData.java
+++ src/main/java/org/gtreimagined/gt5r/data/ClientData.java
@@ -5,12 +5,10 @@ import org.gtreimagined.gtlib.GTAPI;
 import org.gtreimagined.gtlib.gui.container.ContainerMachine;
 import org.gtreimagined.gtlib.gui.container.ContainerMultiMachine;
 import net.minecraft.client.gui.screens.MenuScreens;
-import org.gtreimagined.gt5r.gui.ScreenCoalBoiler;
 import org.gtreimagined.gt5r.gui.ScreenFusionReactor;
 
 public class ClientData {
     public final static MenuScreens.ScreenConstructor SCREEN_FUSION_REACTOR = GTAPI.register(MenuScreens.ScreenConstructor.class, "fusion_reactor", GT5Reimagined.ID,(MenuScreens.ScreenConstructor)(a, b, c) -> new ScreenFusionReactor<>((ContainerMultiMachine) a,b,c));
-    public static final MenuScreens.ScreenConstructor SCREEN_COAL = GTAPI.register(MenuScreens.ScreenConstructor.class, "coal", GT5Reimagined.ID, (MenuScreens.ScreenConstructor)(a, b, c) -> new ScreenCoalBoiler((ContainerMachine) a,b,c));
 
     public static void init() {
     }
