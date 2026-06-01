548119b32b85b08a123e88eaab9d4eedae9f3384
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-05-31 22:09:10 -0400

    added widgets to robot arm

--- src/main/java/org/gtreimagined/gt5r/cover/CoverRobotArm.java
+++ src/main/java/org/gtreimagined/gt5r/cover/CoverRobotArm.java
@@ -1,5 +1,18 @@
 package org.gtreimagined.gt5r.cover;
 
+import brachy.modularui.api.drawable.Text;
+import brachy.modularui.drawable.ItemDrawable;
+import brachy.modularui.factory.SidedPosGuiData;
+import brachy.modularui.screen.ModularPanel;
+import brachy.modularui.screen.UISettings;
+import brachy.modularui.value.sync.EnumSyncValue;
+import brachy.modularui.value.sync.IntSyncValue;
+import brachy.modularui.value.sync.PanelSyncManager;
+import brachy.modularui.widgets.ButtonWidget;
+import brachy.modularui.widgets.CycleButtonWidget;
+import brachy.modularui.widgets.TextWidget;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.world.item.Items;
 import org.gtreimagined.gtlib.blockentity.BlockEntityMachine;
 import org.gtreimagined.gtlib.capability.ICoverHandler;
 import org.gtreimagined.gtlib.capability.machine.MachineItemHandler;
@@ -9,6 +22,8 @@ import org.gtreimagined.gtlib.gui.SlotType;
 import org.gtreimagined.gtlib.gui.event.GuiEvents;
 import org.gtreimagined.gtlib.gui.event.IGuiEvent;
 import org.gtreimagined.gtlib.machine.Tier;
+import org.gtreimagined.gtlib.mui.GTGuiTextures;
+import org.gtreimagined.gtlib.mui.GTMuiUtils;
 import org.gtreimagined.gtlib.util.Utils;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
@@ -27,20 +42,70 @@ public class CoverRobotArm extends CoverBasicTransport {
         super(source, tier, side, factory);
     }
 
-    protected void addButtons() {
-        addGuiCallback(t -> {
-            t.addCycleButton(70, 16, 16, 16, h -> ((CoverBasicRedstone)h).redstoneMode.ordinal(), true, i -> "tooltip.gt5r.redstone_mode." + i, ButtonOverlay.TORCH_OFF, ButtonOverlay.TORCH_ON, ButtonOverlay.REDSTONE);
-            t.addCycleButton(88, 16, 16, 16, h -> ((CoverRobotArm)h).exportMode.ordinal(), true, i -> "tooltip.gt5r.export_mode." + i, ButtonOverlay.EXPORT, ButtonOverlay.IMPORT, ButtonOverlay.EXPORT_IMPORT, ButtonOverlay.IMPORT_EXPORT);
-            t.addTextButton(70, 53, 36, 12, h -> ((CoverRobotArm)h).slot, i -> Utils.literal("Slot: ").append("" + i), 0, true);
-            t.addButton(61,34, ButtonOverlay.MINUS, true);
-            t.addButton(97,34, ButtonOverlay.PLUS, true);
-            /*t.addWidget(SyncableTextWidget.build(i -> {
-                CoverRobotArm robotArm = (CoverRobotArm) i;
-                if (robotArm.slotLimit == 0) return "N/A";
-                return String.valueOf(robotArm.slotLimit);
-            }, 4210752, true).setSize(78, 38, 18, 18));*/
-            //t.addWidget(TextBoxWidget.build((g, s) -> test = s, g -> test).setSize(25, 33, 36, 18));
+    @Override
+    public void addWidgets(ModularPanel<?> modularPanel, SidedPosGuiData sidedPosGuiData, PanelSyncManager syncManager, UISettings uiSettings) {
+        syncManager.syncValue("redstone_mode", new EnumSyncValue<>(RedstoneMode.class, () -> this.redstoneMode, e -> this.redstoneMode = e).allowC2S());
+        syncManager.syncValue("export_mode", new EnumSyncValue<>(ImportExportMode.class, () -> this.exportMode, e -> this.exportMode = e).allowC2S());
+        syncManager.registerSyncedAction("buttonEvent", packet -> {
+            this.onGuiEvent(GuiEvents.EXTRA_BUTTON.factory().apply(GuiEvents.EXTRA_BUTTON, packet), syncManager.getPlayer());
         });
+        syncManager.syncValue("slotLimit", new IntSyncValue(() -> this.slotLimit));
+        syncManager.syncValue("slot", new IntSyncValue(() -> this.slot));
+        modularPanel.child(new CycleButtonWidget()
+                .pos(70, 16).size(16, 16)
+                .stateCount(3).syncHandler("redstone_mode")
+                .stateOverlay(RedstoneMode.NORMAL, GTGuiTextures.TORCH_OFF)
+                .stateOverlay(RedstoneMode.INVERTED, GTGuiTextures.TORCH_ON)
+                .stateOverlay(RedstoneMode.NO_WORK, new ItemDrawable(Items.REDSTONE))
+                .addTooltip(0, Text.lang("tooltip.gt5r.redstone_mode.0"))
+                .addTooltip(1, Text.lang("tooltip.gt5r.redstone_mode.1"))
+                .addTooltip(2, Text.lang("tooltip.gt5r.redstone_mode.2")));
+        modularPanel.child(new CycleButtonWidget()
+                .pos(88, 16).size(16, 16)
+                .stateCount(4).syncHandler("export_mode")
+                .stateOverlay(ImportExportMode.EXPORT, GTGuiTextures.EXPORT)
+                .stateOverlay(ImportExportMode.IMPORT, GTGuiTextures.IMPORT)
+                .stateOverlay(ImportExportMode.EXPORT_IMPORT, GTGuiTextures.EXPORT_IMPORT)
+                .stateOverlay(ImportExportMode.IMPORT_EXPORT, GTGuiTextures.IMPORT_EXPORT)
+                .addTooltip(0, Text.lang("tooltip.gt5r.export_mode.0"))
+                .addTooltip(1, Text.lang("tooltip.gt5r.export_mode.1"))
+                .addTooltip(2, Text.lang("tooltip.gt5r.export_mode.2"))
+                .addTooltip(3, Text.lang("tooltip.gt5r.export_mode.3")));
+        modularPanel.child(new ButtonWidget<>()
+                .overlay(Text.dynamic(() -> {
+                    int slot = GTMuiUtils.getSyncedValue("slot", Integer.class, syncManager.getModularSyncManager()).orElse(0);
+                    return Utils.literal("Slot: " + slot);
+                }))
+                .onMousePressed((context, mouseButton) -> {
+                    syncManager.callSyncedAction("buttonEvent", packet -> {
+                        packet.writeVarIntArray(new int[]{Screen.hasShiftDown() ? 1 : 0, 2});
+                    });
+                    return true;
+                })
+                .size(36, 12).pos(70, 53));
+        modularPanel.child(new ButtonWidget<>()
+                .overlay(GTGuiTextures.MINUS)
+                .onMousePressed((context, mouseButton) -> {
+                    syncManager.callSyncedAction("buttonEvent", packet -> {
+                        packet.writeVarIntArray(new int[]{Screen.hasShiftDown() ? 1 : 0, 3});
+                    });
+                    return true;
+                })
+                .size(16).pos(61, 34));
+        modularPanel.child(new ButtonWidget<>()
+                .overlay(GTGuiTextures.PLUS)
+                .onMousePressed((context, mouseButton) -> {
+                    syncManager.callSyncedAction("buttonEvent", packet -> {
+                        packet.writeVarIntArray(new int[]{Screen.hasShiftDown() ? 1 : 0, 4});
+                    });
+                    return true;
+                })
+                .size(16).pos(97, 34));
+        modularPanel.child(new TextWidget<>(() -> {
+            int fluidLimit = GTMuiUtils.getSyncedValue("slotLimit", Integer.class, syncManager.getModularSyncManager()).orElse(0);
+            if (fluidLimit == 0) return Text.str("N/A");
+            return Text.str(String.valueOf(fluidLimit)).color(4210752);
+        }).pos(79, 34).size(18, 18));
     }
 
     @Override
