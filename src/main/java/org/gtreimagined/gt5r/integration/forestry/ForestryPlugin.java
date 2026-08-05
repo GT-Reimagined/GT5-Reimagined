7918430dc988a39b5d8a17ba939b849db1323b72
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-08-04 20:30:50 -0400

    added gt rubber to forestry multifarm

--- src/main/java/org/gtreimagined/gt5r/integration/forestry/ForestryPlugin.java
+++ src/main/java/org/gtreimagined/gt5r/integration/forestry/ForestryPlugin.java
@@ -1,13 +1,19 @@
 package org.gtreimagined.gt5r.integration.forestry;
 
+import forestry.api.farming.ForestryFarmTypes;
 import forestry.api.plugin.IApicultureRegistration;
 import forestry.api.plugin.IFarmingRegistration;
 import forestry.api.plugin.IForestryPlugin;
 import forestry.api.plugin.IGeneticRegistration;
+import forestry.farming.logic.farmables.FarmableSapling;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import org.gtreimagined.gt5r.GT5Reimagined;
 import org.gtreimagined.gt5r.data.GT5RItems;
+import org.gtreimagined.gtcore.data.GTCoreBlocks;
 import org.gtreimagined.gtcore.data.GTCoreItems;
+
+import java.util.List;
 
 public class ForestryPlugin implements IForestryPlugin {
     @Override
@@ -28,5 +34,8 @@ public class ForestryPlugin implements IForestryPlugin {
     @Override
     public void registerFarming(IFarmingRegistration farming) {
         farming.registerFertilizer(GTCoreItems.Fertilizer, 500);
+        farming.modifyFarmType(ForestryFarmTypes.ARBOREAL, f -> {
+            f.addWindfallFarmable(GTCoreBlocks.RUBBER_SAPLING.asItem(), FarmableSapling::new, b -> b.addWindfall(List.of(Items.STICK, GTCoreItems.StickyResin)));
+        });
     }
 }
