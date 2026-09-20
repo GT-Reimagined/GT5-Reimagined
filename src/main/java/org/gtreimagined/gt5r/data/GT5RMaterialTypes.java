9e133f5ec34d342d576c0e53a847197781d42292
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-09-20 19:58:16 -0400

    updated gt lib

--- src/main/java/org/gtreimagined/gt5r/data/GT5RMaterialTypes.java
+++ src/main/java/org/gtreimagined/gt5r/data/GT5RMaterialTypes.java
@@ -9,30 +9,28 @@ import static org.gtreimagined.gtlib.data.GTMaterialTypes.*;
 
 public class GT5RMaterialTypes {
 
-    public static final MaterialTypeItem<?> TURBINE_BLADE = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("turbine_blade", 2, true, (Ref.U * 3) + (Ref.U8 * 2)));//.unSplitName();
-    public static final MaterialTypeItem<?> HUGE_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("huge_broken_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 8));
-    public static final MaterialTypeItem<?> LARGE_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("large_broken_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 6));
-    public static final MaterialTypeItem<?> BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("broken_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 4));
-    public static final MaterialTypeItem<?> SMALL_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("small_broken_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 2));
-    public static final MaterialTypeItem<?> HUGE_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("huge_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 16, ItemTurbineRotor::new));
-    public static final MaterialTypeItem<?> LARGE_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("large_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 12, ItemTurbineRotor::new));
-    public static final MaterialTypeItem<?> TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 8, ItemTurbineRotor::new));
-    public static final MaterialTypeItem<?> SMALL_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("small_turbine_rotor", 2, true, TURBINE_BLADE.getUnitValue() * 4, ItemTurbineRotor::new));
-    public static final MaterialTypeItem<?> BOULE = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("boule", 2, true, Ref.U * 4));
-    public static final MaterialTypeItem<?> CHAMBER = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("chamber", 2, true, Ref.U * 7));
+    public static final MaterialTypeItem<?> TURBINE_BLADE = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("turbine_blade", true, (Ref.U * 3) + (Ref.U8 * 2)));//.unSplitName();
+    public static final MaterialTypeItem<?> HUGE_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("huge_broken_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 8));
+    public static final MaterialTypeItem<?> LARGE_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("large_broken_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 6));
+    public static final MaterialTypeItem<?> BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("broken_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 4));
+    public static final MaterialTypeItem<?> SMALL_BROKEN_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("small_broken_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 2));
+    public static final MaterialTypeItem<?> HUGE_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("huge_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 16, ItemTurbineRotor::new));
+    public static final MaterialTypeItem<?> LARGE_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("large_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 12, ItemTurbineRotor::new));
+    public static final MaterialTypeItem<?> TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 8, ItemTurbineRotor::new));
+    public static final MaterialTypeItem<?> SMALL_TURBINE_ROTOR = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("small_turbine_rotor", true, TURBINE_BLADE.getUnitValue() * 4, ItemTurbineRotor::new));
+    public static final MaterialTypeItem<?> BOULE = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("boule", true, Ref.U * 4));
+    public static final MaterialTypeItem<?> CHAMBER = GTAPI.register(MaterialTypeItem.class, new MaterialTypeItem<>("chamber", true, Ref.U * 7));
 
     public static void init(){
-        BOULE.setIgnoreTextureSets();
-        CHAMBER.setIgnoreTextureSets();
-        TURBINE_BLADE.setLang(m -> m.getDisplayNameString() + " Turbine Blade").unSplitName().setIgnoreTextureSets();
-        SMALL_BROKEN_TURBINE_ROTOR.setLang(m -> "Broken Small " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        BROKEN_TURBINE_ROTOR.setLang(m -> "Broken " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        LARGE_BROKEN_TURBINE_ROTOR.setLang(m -> "Broken Large " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        HUGE_BROKEN_TURBINE_ROTOR.setLang(m -> "Broken Huge " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        SMALL_TURBINE_ROTOR.setLang(m -> "Small " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        TURBINE_ROTOR.setLang(m -> m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        LARGE_TURBINE_ROTOR.setLang(m -> "Large " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
-        HUGE_TURBINE_ROTOR.setLang(m -> "Huge " + m.getDisplayNameString() + " Turbine Rotor").unSplitName().setIgnoreTextureSets();
+        TURBINE_BLADE.lang(m -> m.getDisplayNameString() + " Turbine Blade").unSplitName();
+        SMALL_BROKEN_TURBINE_ROTOR.lang(m -> "Broken Small " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        BROKEN_TURBINE_ROTOR.lang(m -> "Broken " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        LARGE_BROKEN_TURBINE_ROTOR.lang(m -> "Broken Large " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        HUGE_BROKEN_TURBINE_ROTOR.lang(m -> "Broken Huge " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        SMALL_TURBINE_ROTOR.lang(m -> "Small " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        TURBINE_ROTOR.lang(m -> m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        LARGE_TURBINE_ROTOR.lang(m -> "Large " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
+        HUGE_TURBINE_ROTOR.lang(m -> "Huge " + m.getDisplayNameString() + " Turbine Rotor").unSplitName();
         SMALL_BROKEN_TURBINE_ROTOR.dependents(TURBINE_BLADE);
         BROKEN_TURBINE_ROTOR.dependents(TURBINE_BLADE);
         LARGE_BROKEN_TURBINE_ROTOR.dependents(TURBINE_BLADE);
--- src/main/java/org/gtreimagined/gt5r/integration/tfc/data/TFCMaterialTypes.java
+++ src/main/java/org/gtreimagined/gt5r/integration/tfc/data/TFCMaterialTypes.java
@@ -1,27 +1,26 @@
 package org.gtreimagined.gt5r.integration.tfc.data;
 
 import org.gtreimagined.gtlib.material.MaterialTypeItem;
-import org.gtreimagined.gtlib.util.Utils;
 
 import static org.gtreimagined.gtlib.Ref.U;
 import static org.gtreimagined.gtlib.data.GTMaterialTypes.*;
 
 public class TFCMaterialTypes {
 
-    public static final MaterialTypeItem<?> DOUBLE_INGOT = new MaterialTypeItem<>("double_ingot", 2, true, U * 2);
-    public static final MaterialTypeItem<?> SHEET = new MaterialTypeItem<>("sheet", 2, true, U * 2);
-    public static final MaterialTypeItem<?> CHISEL_HEAD = new MaterialTypeItem<>("chisel_head", 2, true, U);
-    public static final MaterialTypeItem<?> MACE_HEAD = new MaterialTypeItem<>("mace_head", 2, true, U * 2);
-    public static final MaterialTypeItem<?> PROPICK_HEAD = new MaterialTypeItem<>("propick_head", 2, true, U);
-    public static final MaterialTypeItem<?> JAVELIN_HEAD = new MaterialTypeItem<>("javelin_head", 2, true, U);
+    public static final MaterialTypeItem<?> DOUBLE_INGOT = new MaterialTypeItem<>("double_ingot", true, U * 2);
+    public static final MaterialTypeItem<?> SHEET = new MaterialTypeItem<>("sheet", true, U * 2);
+    public static final MaterialTypeItem<?> CHISEL_HEAD = new MaterialTypeItem<>("chisel_head", true, U);
+    public static final MaterialTypeItem<?> MACE_HEAD = new MaterialTypeItem<>("mace_head", true, U * 2);
+    public static final MaterialTypeItem<?> PROPICK_HEAD = new MaterialTypeItem<>("propick_head", true, U);
+    public static final MaterialTypeItem<?> JAVELIN_HEAD = new MaterialTypeItem<>("javelin_head", true, U);
 
     public static void init(){
-        JAVELIN_HEAD.unSplitName().setLang(UNSPLIT_FUNCTION).setIgnoreTextureSets();
-        PROPICK_HEAD.unSplitName().setLang((t, m) -> m.getDisplayNameString() + " " + "Prospector's Pick Head").setIgnoreTextureSets();
-        CHISEL_HEAD.unSplitName().setLang(UNSPLIT_FUNCTION).setIgnoreTextureSets();
-        MACE_HEAD.unSplitName().setLang(UNSPLIT_FUNCTION).setIgnoreTextureSets();
-        DOUBLE_INGOT.unSplitName().setLang(UNSPLIT_FUNCTION);
-        SHEET.setIgnoreTextureSets().dependents(DOUBLE_INGOT);
+        JAVELIN_HEAD.unSplitName().lang(UNSPLIT_FUNCTION);
+        PROPICK_HEAD.unSplitName().lang((t, m) -> m.getDisplayNameString() + " " + "Prospector's Pick Head");
+        CHISEL_HEAD.unSplitName().lang(UNSPLIT_FUNCTION);
+        MACE_HEAD.unSplitName().lang(UNSPLIT_FUNCTION);
+        DOUBLE_INGOT.unSplitName().lang(UNSPLIT_FUNCTION);
+        SHEET.dependents(DOUBLE_INGOT);
         PICKAXE_HEAD.setUnitValue(U);
         AXE_HEAD.setUnitValue(U);
         SHOVEL_HEAD.setUnitValue(U);
